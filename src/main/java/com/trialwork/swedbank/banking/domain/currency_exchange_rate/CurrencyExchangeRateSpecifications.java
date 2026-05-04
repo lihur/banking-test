d501a346cf5cc34fc27472308950e7b465c5c0ee
lihur <pihlam2gi@gmail.com>
2026-05-04 09:33:36 +0300

    Update CurrencyExchangeRateSpecifications.java

--- src/main/java/com/trialwork/swedbank/banking/domain/currency_exchange_rate/CurrencyExchangeRateSpecifications.java
+++ src/main/java/com/trialwork/swedbank/banking/domain/currency_exchange_rate/CurrencyExchangeRateSpecifications.java
@@ -4,7 +4,7 @@ import lombok.*;
 import org.springframework.data.jpa.domain.Specification;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CurrencyExchangeRateSpecifications {
+public final class CurrencyExchangeRateSpecifications {
 
     public static Specification<CurrencyExchangeRate> byKey(final String key) {
         return (root, query, cb) -> cb.equal(root.get(CurrencyExchangeRate_.key), key);
